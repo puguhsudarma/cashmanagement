12ffc1d1e68154bc893fb9f74c7da7fc5868ffb1
Reroet <wayanpuguhsudarma@gmail.com>
2018-06-03 02:17:40 +0800

    init commit, install native dep

---
+++ android/app/src/main/java/com/cashmanagement/MainActivity.java
@@ -0,0 +1,36 @@
+package com.cashmanagement;
+
+import com.reactnativenavigation.controllers.SplashActivity;
+import com.imagepicker.permissions.OnImagePickerPermissionsCallback; // <- add this import
+import com.facebook.react.modules.core.PermissionListener; // <- add this import
+import android.content.Intent; // <--- import
+import android.content.res.Configuration; // <--- import
+
+public class MainActivity extends SplashActivity implements OnImagePickerPermissionsCallback {
+    private PermissionListener listener; // <- add this attribute
+
+    @Override
+    public void setPermissionListener(PermissionListener listener)
+    {
+        this.listener = listener;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
+    {
+        if (listener != null)
+        {
+            listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        Intent intent = new Intent("onConfigurationChanged");
+        intent.putExtra("newConfig", newConfig);
+        this.sendBroadcast(intent);
+    }
+}
---
+++ android/app/src/main/java/com/cashmanagement/MainApplication.java
@@ -0,0 +1,38 @@
+package com.cashmanagement;
+
+import com.facebook.react.ReactPackage;
+import com.reactnativenavigation.NavigationApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.imagepicker.ImagePickerPackage;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends NavigationApplication {
+  @Override
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+            new VectorIconsPackage(),
+            new ImagePickerPackage(),
+            new ImageResizerPackage()
+    );
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
+  }
+}
